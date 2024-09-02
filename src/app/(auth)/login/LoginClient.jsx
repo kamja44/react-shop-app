cd8435fb702756a8ad462ec89ce6d2b9f4963b19
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-03 03:37:37 +0900

    Loader Component

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import LogoPath from "@/assets/colorful.svg";
 import { useRouter } from "next/navigation";
 import styles from "./Auth.module.scss";
+import Loader from "@/components/loader/Loader";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
@@ -25,23 +26,26 @@ const LoginClient = () => {
   };
 
   return (
-    <section className={styles.page}>
-      <div className={styles.container}>
-        <h1 className={styles.logo}>
-          <Image src={LogoPath} alt="logo" />
-        </h1>
-        <form className={styles.form} onSubmit={loginUser}>
-          {/*Input */}Input
-          <div className={styles.group}>
-            {/* 자동 로그인, 비밀번호 수정 */}자동 로그인, 비밀번호 수정{" "}
-          </div>
-          <div className={styles.buttonGroup}>
-            {/* Button */}Button
-            <div>{/* Button */}Button</div>
-          </div>
-        </form>
-      </div>
-    </section>
+    <>
+      <Loader />
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <Image src={LogoPath} alt="logo" />
+          </h1>
+          <form className={styles.form} onSubmit={loginUser}>
+            {/*Input */}Input
+            <div className={styles.group}>
+              {/* 자동 로그인, 비밀번호 수정 */}자동 로그인, 비밀번호 수정{" "}
+            </div>
+            <div className={styles.buttonGroup}>
+              {/* Button */}Button
+              <div>{/* Button */}Button</div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
 

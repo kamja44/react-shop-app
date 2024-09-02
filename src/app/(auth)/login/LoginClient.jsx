23ec567b993f0ac2f1981ce12cbc2f167d1ceee8
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-03 02:50:27 +0900

    Login UI

---
+++ src/app/(auth)/login/LoginClient.jsx
@@ -0,0 +1,48 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import LogoPath from "@/assets/colorful.svg";
+import { useRouter } from "next/navigation";
+import styles from "./Auth.module.scss";
+
+const LoginClient = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAutoLogin, setIsAutoLogin] = useState(false);
+
+  const router = useRouter();
+
+  const redirectUser = () => {
+    router.push("/");
+  };
+  const loginUser = (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+  };
+  const signInWithGoogle = () => {
+    // firebase code
+  };
+
+  return (
+    <section className={styles.page}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>
+          <Image src={LogoPath} alt="logo" />
+        </h1>
+        <form className={styles.form} onSubmit={loginUser}>
+          {/*Input */}Input
+          <div className={styles.group}>
+            {/* 자동 로그인, 비밀번호 수정 */}자동 로그인, 비밀번호 수정{" "}
+          </div>
+          <div className={styles.buttonGroup}>
+            {/* Button */}Button
+            <div>{/* Button */}Button</div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default LoginClient;

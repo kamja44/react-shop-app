2f8d233ba24d232f0645d611991e2806b57add18
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-04 23:00:37 +0900

    divider component

--- src/components/divider/Divider.jsx
+++ src/components/divider/Divider.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
-const Divider = () => {
-  return <div>Divider</div>;
+import styles from "./Divider.module.scss";
+const Divider = ({
+  space = 22,
+  color = "#ccc",
+  className = "",
+  ...restProps
+}) => {
+  const style = {
+    marginTop: space,
+    marginBottom: space,
+    background: color,
+  };
+  return (
+    <div
+      role="presentation"
+      className={styles.line}
+      style={style}
+      {...restProps}
+    />
+  );
 };
 
 export default Divider;

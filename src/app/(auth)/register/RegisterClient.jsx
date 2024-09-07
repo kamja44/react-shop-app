2bd7f302062bdd53481705e3d71508b8648ec23e
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-08 01:18:52 +0900

    signUp

--- src/app/(auth)/register/RegisterClient.jsx
+++ src/app/(auth)/register/RegisterClient.jsx
@@ -9,6 +9,9 @@ import Link from "next/link";
 import LogoPath from "@/assets/colorful.svg";
 import Button from "@/components/button/Button";
 import Image from "next/image";
+import { toast } from "react-toastify";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/firebase";
 
 const RegisterClient = () => {
   const [email, setEmail] = useState("");
@@ -18,7 +21,24 @@ const RegisterClient = () => {
   const router = useRouter();
   const registerUser = (event) => {
     event.preventDefault();
+    if (password !== cPassword) {
+      return toast.error("비밀번호가 일치하지 않습니다.");
+    }
     setIsLoading(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("user", user);
+
+        setIsLoading(false);
+
+        toast.success("등록 성공...");
+        router.push("/login");
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error(error.message);
+      });
   };
   return (
     <>

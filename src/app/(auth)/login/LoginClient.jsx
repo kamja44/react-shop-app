0cd18de7372164c158a8d02585e3d26b609787fe
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-07 18:52:29 +0900

    react toastify

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -10,6 +10,7 @@ import AutoSignInCheckbox from "@/components/autoSignInCheckbox/AutoSignInCheckb
 import Divider from "@/components/divider/Divider";
 import Button from "@/components/button/Button";
 import Link from "next/link";
+import { toast } from "react-toastify";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
@@ -24,6 +25,7 @@ const LoginClient = () => {
   };
   const loginUser = (event) => {
     event.preventDefault();
+    toast.info("성공!");
     setIsLoading(true);
   };
   const signInWithGoogle = () => {

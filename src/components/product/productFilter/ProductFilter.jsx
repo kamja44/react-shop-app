de9a4446886f1a409cddfac024dfb2a67ba65193
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-15 23:40:55 +0900

    filter slice

--- src/components/product/productFilter/ProductFilter.jsx
+++ src/components/product/productFilter/ProductFilter.jsx
@@ -1,6 +1,51 @@
-import React from "react";
+import {
+  FILTER_BY,
+  FILTER_BY_BRAND,
+  FILTER_BY_CATEGORY,
+  FILTER_BY_PRICE,
+} from "@/redux/slice/filterSlice";
+import {
+  selectMaxPrice,
+  selectMinPrice,
+  selectProducts,
+} from "@/redux/slice/productSlice";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const ProductFilter = () => {
+  const [category, setCategory] = useState("All");
+  const [brand, setBrand] = useState("All");
+  const [price, setPrice] = useState("10000");
+
+  const products = useSelector(selectProducts);
+  const minPrice = useSelector(selectMinPrice);
+  const maxPrice = useSelector(selectMaxPrice);
+  const dispatch = useDispatch();
+
+  const allCategories = [
+    "All",
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filterCategories = (category) => {
+    setCategory(category);
+  };
+
+  const allBrands = [
+    "All",
+    ...new Set(products.map((product) => product.brand)),
+  ];
+
+  useEffect(() => {
+    dispatch(FILTER_BY({ products, price, category, brand }));
+  }, [dispatch, products, price, category, brand]);
+
+  const clearFilters = () => {
+    setCategory("All");
+    setBrand("All");
+    setPrice(maxPrice);
+  };
+
   return <div>ProductFilter</div>;
 };
 

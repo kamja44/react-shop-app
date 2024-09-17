1a7385758aa28f4fcdbc7c3b17a38db3dff98f6f
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-17 14:37:22 +0900

    pagination function

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Pagination = () => {
+const Pagination = ({
+  currentPage,
+  productsPerPage,
+  setCurrentPage,
+  totalProducts,
+}) => {
+  const pageNumbers = [];
+
+  const [pageNumberLimit] = useState(3);
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(3);
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const paginateNextPage = () => {
+    setCurrentPage(currentPage + 1);
+
+    if (currentPage + 1 > maxPageNumberLimit) {
+      setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+    }
+  };
+
+  const paginatePrevPage = () => {
+    setCurrentPage(currentPage - 1);
+
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+    }
+  };
+
+  for (
+    let index = 0;
+    index < Math.ceil(totalProducts / productsPerPage);
+    index++
+  ) {
+    pageNumbers.push(index);
+  }
+
   return <div>Pagination</div>;
 };
 

8984afc9c0c356ab107179511737bde3145a6b89
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-17 13:36:16 +0900

    상품 별점

--- src/components/product/productItem/ProductItem.jsx
+++ src/components/product/productItem/ProductItem.jsx
@@ -5,8 +5,19 @@ import Image from "next/image";
 import priceFormat from "@/utils/priceFormat";
 import { Rating } from "react-simple-star-rating";
 import rocketBadgeIcon from "@/assets/badge-rocket.svg";
+import useFetchDocuments from "@/hooks/useFetchDocuments";
 
 const ProductItem = ({ id, name, price, imageURL }) => {
+  // const { documents } = useFetchDocuments("reviews", ["productID", "==", id]);
+
+  // let productRating = 0;
+
+  // documents.map((doc) => {
+  //   productRating = productRating + doc.rate;
+  // });
+
+  // const rating = productRating / documents.length;
+
   const shortenText = (text, n) => {
     if (text.lenfth > n) {
       const shortenedText = text.substring(0, n).concat("...");
@@ -31,8 +42,16 @@ const ProductItem = ({ id, name, price, imageURL }) => {
             원 <Image src={rocketBadgeIcon} alt="로켓배송" />
           </em>
           <div className={styles.rating}>
-            <Rating size={17} initialValue={2} readonly />
-            <span className={styles.ratingCount}>(3)</span>
+            <Rating
+              size={17}
+              initialValue={1}
+              // initialValue={Number.isNaN(rating) ? 0 : rating}
+              readonly
+            />
+            <span className={styles.ratingCount}>
+              {/* ({documents.length}) */}
+              (1)
+            </span>
           </div>
         </div>
       </div>

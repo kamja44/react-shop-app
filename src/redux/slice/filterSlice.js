fb617fc05c003de18b76b5d55ac579e18d5a4537
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-09-17 13:42:07 +0900

    search

--- src/redux/slice/filterSlice.js
+++ src/redux/slice/filterSlice.js
@@ -79,6 +79,16 @@ const filterSlice = createSlice({
       }
       state.filteredProducts = tempProducts;
     },
+    FILTER_BY_SEARCH: (state, action) => {
+      const { products, search } = action.payload;
+
+      const tempProducts = products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(search.toLowerCase()) ||
+          product.category.toLowerCase().includes(search.toLowerCase())
+      );
+      state.filteredProducts = tempProducts;
+    },
   },
 });
 
@@ -88,6 +98,7 @@ export const {
   FILTER_BY_PRICE,
   FILTER_BY,
   SORT_PRODUCTS,
+  FILTER_BY_SEARCH,
 } = filterSlice.actions;
 
 export const selectFilteredProducts = (state) => state.filter.filteredProducts;
